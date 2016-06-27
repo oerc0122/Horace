5ce7f7a465f70ae4d4abdccecb8a6354119f58c4
Alex Buts <alex.buts@stfc.ac.uk>
2016-06-27 17:30:01 +0000

    Re #160 More changes to work on Unix (incomplete)
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/branches/comb_sqw%23160@1258 1cb955da-4463-431d-88dd-2af2f5550de3

--- _LowLevelCode/cpp/combine_sqw/combine_sqw.h
+++ _LowLevelCode/cpp/combine_sqw/combine_sqw.h
@@ -9,20 +9,6 @@
 // $Revision::      $ ($Date::                                              $)" 
 //
 
-// parameters the mex routine uses and accepts in the array of input parameters
-struct ProgParameters {
-    size_t totNumBins;  // total number of bins in files to combine (has to be the same for all files)
-    size_t nBin2read;  // current bin number to read (start from 0 for first bin of the array)
-    size_t pixBufferSize; // the size of the buffer to return combined pixels
-    int log_level;       // the number defines how talkative program is. usually it its > 1 all 
-                         // all diagnostics information gets printed
-    size_t num_log_ticks; // how many times per combine files to print log message about completion percentage
-                          // Default constructor
-    ProgParameters() :totNumBins(0), nBin2read(0),
-        pixBufferSize(10000000), log_level(1), num_log_ticks(100)
-    {};
-};
-
 
 enum readBinInfoOption {
     sumPixInfo,
--- _LowLevelCode/cpp/combine_sqw/nsqw_pix_reader.h
+++ _LowLevelCode/cpp/combine_sqw/nsqw_pix_reader.h
@@ -3,6 +3,21 @@
 
 #include "sqw_reader.h"
 #include "exchange_buffer.h"
+
+// parameters the mex routine uses and accepts in the array of input parameters
+struct ProgParameters {
+    size_t totNumBins;  // total number of bins in files to combine (has to be the same for all files)
+    size_t nBin2read;  // current bin number to read (start from 0 for first bin of the array)
+    size_t pixBufferSize; // the size of the buffer to return combined pixels
+    int log_level;       // the number defines how talkative program is. usually it its > 1 all 
+                         // all diagnostics information gets printed
+    size_t num_log_ticks; // how many times per combine files to print log message about completion percentage
+                          // Default constructor
+    ProgParameters() :totNumBins(0), nBin2read(0),
+        pixBufferSize(10000000), log_level(1), num_log_ticks(100)
+    {};
+};
+
 //-----------------------------------------------------------------------------------------------------------------
 /* Structure (class) supporting the read operations for range of input files and combining the information
 from this files together in the file buffer*/
